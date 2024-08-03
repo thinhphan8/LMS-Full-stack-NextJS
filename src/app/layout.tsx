54f83e8978f918ed6c266ff4e488b90bca6d19a0
SOMETHINGG\thinh <thinhphanvn8499@gmail.com>
2024-08-03 22:29:44 +0700

    Added devcontainer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,29 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type {Metadata} from "next";
+import {Inter} from "next/font/google";
 // import "./globals.css";
 import '@radix-ui/themes/styles.css';
 import {Theme} from "@radix-ui/themes";
+import React from "react";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
-  title: "LMS",
-  description: "A LMS build with NextJS",
+    title: "LMS",
+    description: "A LMS build with NextJS",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-    <Theme>
-      <body className={inter.className}>{children}</body>
-    </Theme>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <Theme>
+                    {children}
+                </Theme>
+            </body>
+        </html>
+    );
 }
