43cb91e48233076f7b3bef8646c9f35521f188ef
SOMETHINGG\thinh <thinhphanvn8499@gmail.com>
2024-08-03 20:59:47 +0700

    Added middleware Update next config

--- src/app/login/messages.tsx
+++ src/app/login/messages.tsx
@@ -3,9 +3,9 @@
 import { useSearchParams } from 'next/navigation'
 
 export default function Messages() {
-    const searchParams = useSearchParams()
-    const error = searchParams.get('error')
-    const message = searchParams.get('message')
+    const searchParams = useSearchParams();
+    const error = searchParams.get('error');
+    const message = searchParams.get('message');
     return (
         <>
             {error && (
@@ -19,5 +19,5 @@ export default function Messages() {
                 </p>
             )}
         </>
-    )
+    );
 }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -48,5 +48,5 @@ export default function Login() {
                 <Messages/>
             </form>
         </div>
-    )
+    );
 }
